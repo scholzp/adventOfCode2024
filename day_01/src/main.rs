00bccb8f2b4124ff9cd76c7e1e6d9bd744758527
Pascal Scholz <pasix@outlook.de>
2024-12-28 19:28:48 +0100

    Add two possible solutions to task 2 of day 2
    The one not using iterators is currently broken.

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -23,6 +23,46 @@ fn task_1(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
     result
 }
 
+fn task_2(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
+    // This is a linear approach to solve the problem
+    let mut distance = 0;
+    let mut start_index = 0;
+    let mut count = 0;
+
+    for l in list1 {
+        if *l != list2[start_index] {
+            if count == 0 {
+                start_index += 1;
+            } else {
+                start_index += count;
+                count = 0;
+            }
+        };
+        if count == 0 {
+            for r in &list2[start_index..] {
+                if r == l {
+                    count += 1
+                } else {
+                    break;
+                };
+            }
+        }
+        distance += (count as i32) * *l;
+    }
+    distance
+}
+
+fn task_2_it(list_l: &[i32], list_r: &[i32]) -> i32 {
+    list_l
+        .iter()
+        .map(|elem| {
+            let count = list_r.iter().filter(|e| *e == elem).count() as i32;
+            let sum = elem * count;
+            sum
+        })
+        .sum()
+}
+
 fn main() {
     let (test_input_1, test_input_2) = read_file("input.input");
     // let mut test_input_1: Vec<i32> = vec![3, 4, 2, 1, 3, 3];
@@ -31,4 +71,6 @@ fn main() {
     // test_input_2.sort();
 
     println!("Input 1: {:#?}", task_1(&test_input_1, &test_input_2));
+    println!("Task 2: {:#?}", task_2(&test_input_1, &test_input_2));
+    println!("Task 2_it: {:#?}", task_2_it(&test_input_1, &test_input_2));
 }
